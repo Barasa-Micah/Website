8f6f7e7f5473758eebfd1c09fc7cc00f62e5464a
Barasa-Micah <barasamica@gmail.com>
2023-06-11 15:01:01 +0300

    Add the event listener on the script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+const bioElement = document.getElementById("bio");
+const showMoreButton = document.getElementById("Show-more");
+
+
+const fullBio = "I am a software engineer and full stack web developer <br>with an experience web design and maintaining   <br> systems evaluating and testing of new software <br> programs and optimizing software for speed and scalability";
+
+bioElement.style.display = "none";
+
+showMoreButton.addEventListener("click",function(){
+    if(bioElement.style.display==="none"){
+        bioElement.innerHTML=fullBio;
+        bioElement.style.display="block";
+        showMoreButton.textContent="Show Less";
+    } else{
+        bioElement.style.display= "none";
+        showMoreButton.textContent= "Show More"
+    }
+}) 
